d1fc5b06b0563022085bde04ed9fe9695410c0c5
txmxtx <dxxeon@ewhain.net>
2026-03-19 18:41:13 +0900

    feat

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Game from "./pages/Game";
 import Scoreboard from "./pages/Scoreboard";
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
